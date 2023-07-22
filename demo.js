a3698f7d7d4717d80f8b0d1dc65e38dd0acd2c16
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-07-22 20:33:21 +0530

    Your commit message here

---
+++ demo.js
@@ -0,0 +1,12 @@
+console.log('yes');
+
+/*
+echo "# gitgit" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/divyanshu4757/gitgit.git
+git push -u origin main
+
+*/
