a37ca0b14a68f0506cc1b5e18d2651a33f67ff1b
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-07-24 00:20:04 +0530

    local storage scalable

--- local.js
+++ local.js
@@ -5,13 +5,18 @@ form.addEventListener('submit' ,(e)=>{
     e.preventDefault();
     let name = document.getElementById('name');
     let email = document.getElementById('exampleEmail');
+    let phone = document.getElementById('tel');
+
     let obj = {
-        myName:name.value,
-        myEmail:email.value,
+        Name:name.value,
+        Email:email.value,
+        Phone:phone.value,
     }
 
-    localStorage.setItem(name.value ,JSON.stringify(obj));
+    localStorage.setItem(email.value ,JSON.stringify(obj));
     name.value = '';
     email.value ='';
+    phone.value ='';
+
 
 })
