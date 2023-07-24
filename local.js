719bede7373bd18733a645810003f98f2c980f8e
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-07-24 09:54:36 +0530

    edit functionality

--- local.js
+++ local.js
@@ -1,13 +1,14 @@
 
 let form = document.getElementById('form');
 let ul= document.getElementById('add');
+let name = document.getElementById('name');
+let email = document.getElementById('exampleEmail');
+let phone = document.getElementById('tel');
 
 
 form.addEventListener('submit' ,(e)=>{
     e.preventDefault();
-    let name = document.getElementById('name');
-    let email = document.getElementById('exampleEmail');
-    let phone = document.getElementById('tel');
+    
 
     let obj = {
         Name:name.value,
@@ -29,9 +30,13 @@ form.addEventListener('submit' ,(e)=>{
 
     let button =  document.createElement('button');
     button.textContent = "delete";
-    button.className="btn btn-secondary"
+    button.className="btn btn-secondary delete-that"
     li.append(button);
 
+    let edit =  document.createElement('button');
+    edit.textContent = "edit";
+    edit.className="btn btn-success edit"
+    li.append(edit);
 
     ul.appendChild(li);
 
@@ -44,11 +49,29 @@ form.addEventListener('submit' ,(e)=>{
 
 
 ul.addEventListener('click',(e)=>{
-    if(e.target.classList.contains("btn")){
-        let parent = e.target.parentNode;
+    if(e.target.classList.contains("delete-that")){
+        
+       
         const nthElementChild =parent.childNodes[2].textContent;
        // console.log(nthElementChild);
         localStorage.removeItem(nthElementChild);
           e.target.parentNode.remove();
     }   
+
+    if(e.target.classList.contains("edit")){
+        console.log(e.target.classList);
+        let parent = e.target.parentNode;
+        const nthName = parent.childNodes[0].textContent;
+
+        const nthEmail = parent.childNodes[2].textContent;
+
+        const nthNum = parent.childNodes[4].textContent;
+
+        console.log(nthEmail);
+        name.value = nthName;
+        email.value = nthEmail;
+        phone.value = nthNum;
+        localStorage.removeItem(nthEmail);
+        e.target.parentNode.remove();
+    }
 })
